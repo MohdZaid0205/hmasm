0028d1c6de8bc4ed8011de7797bcd9f774900676
Mohd Zaid <mdzaid0205@gmail.com>
2025-12-28 06:24:44 +0000

    add debug information for inputs and read file
    This is supposed to be a pivotal change from previous implementation,
    assembler.c is just there as a proof of concept and testing
    implementation before they are to be added to assembler
    
    next step require development of lexer and a parser.

--- src/core/assembler.c
+++ src/core/assembler.c
@@ -3,14 +3,21 @@
 #include "argparse.h"
 #include "lexer.h"
 #include "isa.h"
+#include "format.h"
 
 int main(int argc, char** argv){
+    
+    DEBUG("This distribution compiles to ISA:%(%s%)\n", assembler_isa_name);
+    DEBUG("This distribution compiles to FMT:%(%s%)\n", assembler_format_name);
+
+    const char* source_file_name = NULL;
+    const char* output_file_name = NULL;
     
     int i = 1;
     while (i < argc){
         switch (argparse_get_type_for(argv[i])){
             case ARGUMENT_OUT:
-                argparse_argument_output(argv[++i]);
+                output_file_name = argparse_argument_output(argv[++i]).value;
                 break;
             case ARGUMENT_FMT:
                 argparse_argument_format(argv[++i]);
@@ -22,7 +29,7 @@ int main(int argc, char** argv){
                 argparse_argument_help("HELP ME");
                 break;
             case ARGUMENT_INP:
-                argparse_argument_input(argv[i]);
+                source_file_name = argparse_argument_input(argv[i]).value;
                 break;
             case ARGUMENT_NONE:
                 return -1;
@@ -33,6 +40,21 @@ int main(int argc, char** argv){
         i++;
     }
 
-    LOG("name of isa:%s\n", assembler_isa_name);
+    if (source_file_name == NULL)
+        exit(-1);
+    DEBUG("looking for file name %(%s%)\n", source_file_name);
+
+    FILE* source = fopen(source_file_name, "r");
+    // exit(-1);
+
+    if (source == NULL)
+        exit(-2);
+
+    unsigned int found = ' ';
+    while(found != EOF){
+        found = fgetc(source);
+        printf("%c", (unsigned char)found);
+    }
+
     return 0;
 }
