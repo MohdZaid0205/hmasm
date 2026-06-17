14144a1744731d84379b6454d9f9964d7396e719
Mohd Zaid <mdzaid0205@gmail.com>
2026-06-17 01:36:43 +0000

    add dumping logic for syntax tree visualization (part 2)

--- src/core/ir.c
+++ src/core/ir.c
@@ -24,3 +24,20 @@ void ir_dump_block(struct BLOCK* blk) {
     }
     free(data);
 }
+
+void ir_dump_directive(struct DIRECTIVE* dir) {
+    switch (dir->type) {
+        case DIRECTIVE_SECTION_T: DEBUG_LN("", IR_DEBUG_DIR_SEC); break;
+        case DIRECTIVE_ALIGN_T:   DEBUG_LN("", IR_DEBUG_DIR_ALG); break;
+        case DIRECTIVE_GLOBAL_T:  DEBUG_LN("", IR_DEBUG_DIR_GLB); break;
+        case DIRECTIVE_EXTERN_T:  DEBUG_LN("", IR_DEBUG_DIR_EXT); break;
+        case DIRECTIVE_ENTRY_T:   DEBUG_LN("", IR_DEBUG_DIR_ENT); break;
+        case DIRECTIVE_INCLUDE_T: DEBUG_LN("", IR_DEBUG_DIR_INC); break;
+        case DIRECTIVE_ERROR_T:   DEBUG_LN("", IR_DEBUG_DIR_ERR); break;
+        default:                  DEBUG_LN("", IR_DEBUG_DIR_UNK); break;
+    }
+    DEBUG_LN("", IR_DEBUG_DIR_TRG, dir->target ? dir->target : "<none>");
+    if (dir->modifier) {
+        DEBUG_LN("", IR_DEBUG_DIR_MOD, dir->modifier);
+    }
+}
